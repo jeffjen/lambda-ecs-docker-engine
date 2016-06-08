74499f3d2182d19bf580bf7b7647ca1d716c22f2
Jeffrey Jen <yihungjen@gmail.com>
2016-06-08 18:31:33 +0000

    UPDATE: gulp task for retrieving vendor binary

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 "use strict"
 
 const gulp = require("gulp");
+
+const chmod = require('gulp-chmod');
+const filter = require('gulp-filter');
+const gunzip = require("gulp-gunzip");
+const rename = require("gulp-rename");
+const request = require("request");
+const source = require("vinyl-source-stream");
+const untar = require("gulp-untar");
 const zip = require("gulp-zip");
 
 const bundle = {
@@ -24,8 +32,28 @@ const bundle = {
     }
 };
 
-gulp.task("bundle", function() {
+gulp.task("bundle", ["vendor"], function() {
     gulp.src(bundle.src, bundle.opts)
         .pipe(zip(bundle.output))
         .pipe(gulp.dest(bundle.dst));
+});
+
+gulp.task("vendor", ["vendor-weave", "vendor-docker"]);
+
+gulp.task("vendor-weave", function() {
+    return request("https://git.io/weave").
+        pipe(source("weave")).
+        pipe(chmod(755)).
+        pipe(gulp.dest("vendor"));
+});
+
+gulp.task("vendor-docker", function() {
+    return request("https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz").
+        pipe(source("docker-latest.tgz")).
+        pipe(gunzip()).
+        pipe(untar()).
+        pipe(filter("docker/docker")).
+        pipe(rename("docker")).
+        pipe(chmod(755)).
+        pipe(gulp.dest("vendor"));
 });
