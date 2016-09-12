178b06ec298484bed5ad9140c5c2ecb94e1032ce
Jeffrey Jen <yihungjen@gmail.com>
2016-09-12 04:57:04 +0000

    Better gulpfile organization

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,78 @@
 "use strict"
 
+const chmod = require("gulp-chmod");
+const del = require("del");
+const filter = require("gulp-filter");
 const gulp = require("gulp");
-
-const chmod = require('gulp-chmod');
-const filter = require('gulp-filter');
 const gunzip = require("gulp-gunzip");
+const lambda = require("gulp-awslambda");
 const rename = require("gulp-rename");
 const request = require("request");
+const shell = require('gulp-shell')
 const source = require("vinyl-source-stream");
 const untar = require("gulp-untar");
 const zip = require("gulp-zip");
 
-const bundle = {
+const dest = {
+    lambda: "dist"
+};
+const paths = {
     src: [
         ".env",
-        "cert/**",
-        "vendor/**",
-        "ecs-docker-engine",
-        "lambda.js",
-        "node_modules/**",
-        "!node_modules/gulp/",
-        "!node_modules/gulp/**",
-        "!node_modules/gulp-*/",
-        "!node_modules/gulp-*/**",
-        "!node_modules/request/",
-        "!node_modules/request/**",
-        "!node_modules/vinyl-source-stream/",
-        "!node_modules/vinyl-source-stream/**",
         "package.json",
-    ],
-    dst: ".",
-    output: "app.zip",
-    opts: {
-        base: "."
-    }
+        "lambda.js",
+        "ecs-docker-engine",
+    ]
 };
 
-gulp.task("bundle-only", function() {
-    gulp.src(bundle.src, bundle.opts)
-        .pipe(zip(bundle.output))
-        .pipe(gulp.dest(bundle.dst));
+gulp.task("clean", function () {
+    return del([ dest.lambda ]);
 });
 
-gulp.task("bundle", ["vendor", "bundle-only"]);
+gulp.task("lambda.cert", function() {
+    return gulp.src(`${process.env.HOME}/.machine/*.pem`).
+        pipe(gulp.dest(`${dest.lambda}/cert`));
+});
 
-gulp.task("vendor", ["vendor-weave", "vendor-docker"]);
+gulp.task("lambda.vendor.docker", function() {
+    return gulp.src("/usr/bin/docker").
+        pipe(gulp.dest(`${dest.lambda}/vendor`));
+});
 
-gulp.task("vendor-weave", function() {
+gulp.task("lambda.vendor.machine", function() {
+    return gulp.src("/usr/local/bin/machine").
+        pipe(gulp.dest(`${dest.lambda}/vendor`));
+});
+
+gulp.task("lambda.vendor.weave", function() {
     return request("https://git.io/weave").
         pipe(source("weave")).
         pipe(chmod(755)).
-        pipe(gulp.dest("vendor"));
+        pipe(gulp.dest(`${dest.lambda}/vendor`));
 });
 
-gulp.task("vendor-docker", function() {
-    return request("https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz").
-        pipe(source("docker-latest.tgz")).
-        pipe(gunzip()).
-        pipe(untar()).
-        pipe(filter("docker/docker")).
-        pipe(rename("docker")).
-        pipe(chmod(755)).
-        pipe(gulp.dest("vendor"));
+gulp.task("lambda.vendor", [ "lambda.vendor.docker", "lambda.vendor.machine", "lambda.vendor.weave" ]);
+
+gulp.task("lambda.npm.src", function () {
+    return gulp.src(paths.src).
+        pipe(gulp.dest(dest.lambda));
+});
+
+gulp.task("lambda.npm", [ "lambda.npm.src" ], shell.task([
+    `cd ${dest.lambda} && npm install --production`
+]));
+
+gulp.task("lambda", [ "lambda.cert", "lambda.vendor", "lambda.npm" ], function() {
+    gulp.src([ `${dest.lambda}/*`, `!${dest.lambda}/app.zip` ]).
+        pipe(zip("app.zip")).
+        pipe(gulp.dest(dest.lambda));
+});
+
+gulp.task("default", [ "lambda" ]);
+
+gulp.task("deploy", [ "lambda" ], function () {
+    let FunctionName = "lambda-ecs-docker-engine";
+    gulp.src([ `${dest.lambda}/*`, `!${dest.lambda}/app.zip` ]).
+        pipe(zip("app.zip")).
+        pipe(lambda(FunctionName, { region: "ap-northeast-1" }));
 });
